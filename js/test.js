0d9819d8aed5ff082988f5479dda46766af63e7b
中川陽介 <yosuke.volleyball@gmail.com>
2020-12-17 05:52:37 +0900

    maybe use it as it is

--- js/test.js
+++ js/test.js
@@ -5,6 +5,9 @@ $(function () {
 
 });
 
+let elem = document.getElementById("history_table");
+elem.innerHTML = '<tr><th>ID</th><th>作成日</th><th>作成者</th></tr>';
+
 const startScanner = () => {
     Quagga.init({
         inputStream: {
@@ -86,5 +89,41 @@ const startScanner = () => {
     //barcode read call back
     Quagga.onDetected(function (result) {
         console.log(result.codeResult.code);
+        check_search(result.codeResult.code);
     });
+}
+
+const pre_list = [
+["0017","X年12月05日","E"],
+["0005","X年12月04日","E"],
+["0002","X年12月03日","E"],
+["0007","X年12月05日","F"],
+["0003","X年12月04日","F"],
+["0085","X年12月03日","F"],
+["0019","X年12月05日","G"],
+["0011","X年12月04日","G"],
+["0046","X年12月03日","G"],
+["0029","X年12月05日","H"],
+["0091","X年12月04日","H"],
+["0057","X年12月03日","H"],
+["0638","X年11月11日","S"]
+];
+
+const transpose = a => a[0].map((_, c) => a.map(r => r[c]));
+const trans_list = transpose(pre_list);
+
+let read = [];
+let html_hist = '';
+
+let check_search = (result) => {
+  if(result!==read[read.length-1] && result.length===4){
+      let num= trans_list[0].indexOf(result);
+      if(num!== -1){
+        read.push(result);
+        html_hist = '<tr><th>'+pre_list[num][0]+'</th><th>'+pre_list[num][1]+'</th><th>'+pre_list[num][2]+'</th></tr>' + html_hist;
+        elem.innerHTML = '<tr><th>ID</th><th>作成日</th><th>作成者</th></tr>'+html_hist;
+      }
+
+  }
+
 }
